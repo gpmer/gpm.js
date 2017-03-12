4d2bcfd91ac38ca27fbb96d719052f32d6634d2b
axetroy <troy450409405@gmail.com>
2017-03-12 20:39:30 +0800

    test: complete add＆list test

--- test/gpm.test.js
+++ test/gpm.test.js
@@ -12,8 +12,15 @@ const gitUrlParse = require("git-url-parse");
 const CONFIG = require('../lib/config');
 const prepare = require('../lib/prepare');
 const add = require('../lib/command/add');
+const list = require('../lib/command/list');
 
 const {home, root, temp, config} = CONFIG.paths;
+
+test.before(async function (t) {
+  await fs.emptydirAsync(home);
+  await fs.removeAsync(home);
+  t.pass();
+});
 
 test.serial.beforeEach(async function (t) {
   await fs.ensureDir(home);
@@ -38,7 +45,7 @@ test.serial('prepare', async(t) => {
   t.pass();
 });
 
-test.serial('add', async(t) => {
+test.serial('add & list', async(t) => {
   const url = 'https://github.com/gpmer/gpm-empty-repository-for-test.git';
   const argv = {repo: url}, options = {force: true, quiet: true};
   const gitInfo = gitUrlParse(url);
@@ -55,5 +62,18 @@ test.serial('add', async(t) => {
   await fs.ensureDirAsync(sourceDir);
   await fs.ensureDirAsync(ownerDir);
   await fs.ensureDirAsync(repoDir);
+
+  const result = await list({}, {nolog: true});
+
+  const source = 'github.com'.red;
+  const owner = 'gpmer'.yellow;
+  const name = 'gpm-empty-repository-for-test'.green;
+
+  t.deepEqual(_.keys(result), [source]);
+  t.deepEqual(_.keys(result[source]), [owner]);
+  t.deepEqual(_.keys(result[source][owner]), [name]);
+  t.deepEqual(result[source][owner][name], repoDir.white);
+
   t.pass();
+
 });
