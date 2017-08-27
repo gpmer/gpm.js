ff471f6998164e4767ddf5cfe0af27a3f5c51320
axetroy <troy450409405@gmail.com>
2017-08-28 00:13:08 +0800

    refactor: list

--- app/command/list.ts
+++ app/command/list.ts
@@ -12,7 +12,7 @@ import { __ } from 'i18n';
 const logger = log4js.getLogger('LIST');
 import config from '../config';
 import { normalizePath } from '../utils';
-import registry from '../registry';
+import registry, { Target$, Json$ } from '../registry';
 
 interface Argv$ {
   key: string;
@@ -24,29 +24,31 @@ interface Options$ {
   force?: boolean;
 }
 
-async function ls(key: string, options: Options$) {
+async function ls(key: string, options: Options$): Promise<Json$> {
   if (registry.isEmpty)
     return logger.warn(__('commands.list.log.warn_empty_registry'));
 
-  let repositories = key ? registry.find(key) : registry.repositories.slice();
+  let repositories: Target$[] = key
+    ? registry.find(key)
+    : registry.repositories.slice();
 
   if (_.isEmpty(repositories))
     return logger.info(__('commands.list.log.err_not_found'));
 
-  const result = registry.toJson(
+  const result: Json$ = registry.toJson(
     repositories,
     _.extend({ color: true }, options)
   );
 
-  const basePath = path.join(config.paths.home, config.defaults.base);
+  const basePath: string = path.join(config.paths.home, config.defaults.base);
 
-  const ROOT = normalizePath(basePath, options).white;
+  const ROOT: string = normalizePath(basePath, options).white;
 
   !options.nolog &&
     process.stdout.write(prettyjson.render({ [ROOT]: result }) + '\n');
   return result;
 }
 
-export default async function(argv: Argv$, options: Options$) {
+export default async function(argv: Argv$, options: Options$): Promise<Json$> {
   return await ls(argv.key, options);
 }
